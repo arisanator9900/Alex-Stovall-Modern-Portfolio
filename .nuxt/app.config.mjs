a634270c2f796e6f6f2fc8ff61058fa19593bb9e
Alexander Stovall <96804792+arisanator9900@users.noreply.github.com>
2023-01-22 15:45:04 -0600

    set up repository

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/alexanderstovall/Documents/GitHub/Alex-Stovall-Modern-Portfolio/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ components/Selector.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul id="buttons05" class="style1 buttons">
+      <li><a 
+              class="button n01">Download</a></li>
+  </ul>
+  <ul id="buttons01" class="style2 buttons">
+      <li><a class="button n01">Buy
+              print</a></li>
+  </ul>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style >
+  
+</style>
