16ea1feb224f802d5cab5fead83a96c707fd58a7
Alexander Stovall <96804792+arisanator9900@users.noreply.github.com>
2023-01-22 16:06:03 -0600

    removed All

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/alexanderstovall/Documents/GitHub/Alex-Stovall-Modern-Portfolio/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
