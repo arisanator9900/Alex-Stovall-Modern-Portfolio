4fca4a593c9d94e4299815e197d12c30bc8d80e4
Alexander Stovall <96804792+arisanator9900@users.noreply.github.com>
2023-01-22 18:17:14 -0600

    edited title block

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Title from './components/Title'
+import './App.css'
+import { Projects } from './components/Projects'
+import { Fireworks } from './components/Fireworks'
+
+
+function App() {
+
+  return (
+    <div>
+      <Fireworks/>
+        {/* <Title/> */}
+      <Projects/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Projects = () => {
+  return (
+    <div>
+        Projects
+        <div className="container">Project1</div>
+        <div className="container">Project2</div>
+        <div className="container">Project3</div>
+    </div>
+  )
+}
